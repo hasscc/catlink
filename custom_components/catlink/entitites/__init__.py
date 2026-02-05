b706b47fea08c907f126ea7891972f9fc49342f1
Miloš Ljubenović <milos.ljubenovic@adapptiv.uk>
2026-02-05 14:00:00 +0000

    Remove deprecated modules and fix entities folder name

--- custom_components/catlink/entitites/__init__.py
+++
@@ -1,9 +0,0 @@
-"""The catlink component entities."""
-
-from .catlink import CatlinkEntity
-from .catlink_binary import CatlinkBinaryEntity
-
-__all__ = [
-    "CatlinkEntity",
-    "CatlinkBinaryEntity",
-]
